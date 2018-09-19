4c712f5175968242cc0d7043b228f80137dad4a7
Lucas Rodrigues Teixeira Nunes <lucas.nunes@radixeng.com.br>
2018-09-19 16:29:30 -0300

    Mudanças de legibilidade no código

--- main.py
+++ main.py
@@ -6,23 +6,33 @@ import numpy as np
 import matplotlib.pyplot as plt
 from copy import deepcopy
 
+#Readability constants
+POS_X = 0
+POS_Y = 1
+ID = 2
+SERVER = 3
+BLOOM_FILTER = 4
+HASH_SET = 5
+NEIGHBORS = 6
+PLAYERS = 7
+SERVERS = 8
 
 # Generates random player positions
 def generate_players(number_of_players):
     player_list = []
     for i in range(number_of_players):
         player = {}
-        player['pos_x'] = uniform(0, MAP_XMAX)
-        player['pos_y'] = uniform(0, MAP_YMAX)
-        player['id'] = i
-        create_spatial_index(player['id'], player['pos_x'], player['pos_y'])
+        player[POS_X] = uniform(0, MAP_XMAX)
+        player[POS_Y] = uniform(0, MAP_YMAX)
+        player[ID] = i
+        create_spatial_index(player[ID], player[POS_X], player[POS_Y])
         player_list.append(player)
     return player_list
 
 
-def create_servers(server_load, number_of_servers):
+def create_servers(number_of_servers):
     return [
-        {'bloom_filter': BloomFilter(capacity=server_load, error_rate=0.001)}
+        {HASH_SET: set()}
         for i in range(number_of_servers)]
 
 
@@ -43,10 +53,13 @@ def allocate_player_to_server_hashing(player_list, server_list):
     number_of_servers = len(server_list)
     for player in player_list:
         try:
-            server_list[player['id'] % number_of_servers]['bloom_filter'].add(player['id'])
-            player['server'] = player['id'] % number_of_servers
-            if verbose:
-                print("Player {} allocated in server {}".format(player['id'], player['server']))
+            if len(server_list[player[ID]][HASH_SET]) < server_capacity:
+                server_list[player[ID] % number_of_servers][HASH_SET].add(player[ID])
+                player[SERVER] = player[ID] % number_of_servers
+                if verbose:
+                    print("Player {} allocated in server {}".format(player[ID], player[SERVER]))
+            else:
+                raise IndexError
         except IndexError:
             change_player_server(server_list, player)
 
@@ -55,120 +68,100 @@ def allocate_player_to_server_hashing(player_list, server_list):
 def allocate_player_to_server_equal_partitions(player_list, server_list):
     frontiers = []
     number_of_servers = len(server_list)
-    is_grid = False
-    if number_of_servers < 9:
-        for i in range(number_of_servers - 1):
-            frontiers.append((i + 1) * (MAP_XMAX / number_of_servers))
-        for player in player_list:
-            if player['pos_x'] < frontiers[0]:
-                try:
-                    server_list[0]['bloom_filter'].add(player['id'])
-                    player['server'] = 0
+    for i in range(number_of_servers - 1):
+        frontiers.append((i + 1) * (MAP_XMAX / number_of_servers))
+    for player in player_list:
+        if player[POS_X] < frontiers[0]:
+            try:
+                if len(server_list[0][HASH_SET]) < server_capacity:
+                    server_list[0][HASH_SET].add(player[ID])
+                    player[SERVER] = 0
                     if verbose:
                         print(
-                            "Player {} allocated in server {} - Coordinates({},{}) - Frontier: < {}".format(player['id'], 0,
-                                                                                                            player['pos_x'],
-                                                                                                            player['pos_y'],
+                            "Player {} allocated in server {} - Coordinates({},{}) - Frontier: < {}".format(player[ID], 0,
+                                                                                                            player[POS_X],
+                                                                                                            player[POS_Y],
                                                                                                             frontiers[0]))
-                except IndexError:
-                    change_player_server(server_list, player)
-            elif player['pos_x'] >= frontiers[-1]:
-                try:
-                    server_list[-1]['bloom_filter'].add(player['id'])
-                    player['server'] = server_list.index(server_list[-1])
+                else:
+                    raise IndexError
+            except IndexError:
+                change_player_server(server_list, player)
+
+        elif player[POS_X] >= frontiers[-1]:
+            try:
+                if len(server_list[-1][HASH_SET]) < server_capacity:
+                    server_list[-1][HASH_SET].add(player[ID])
+                    player[SERVER] = server_list.index(server_list[-1])
                     if verbose:
-                        print("Player {} allocated in server {} - Coordinates({},{}) - Frontier: > {}".format(player['id'],
+                        print("Player {} allocated in server {} - Coordinates({},{}) - Frontier: > {}".format(player[ID],
                                                                                                               number_of_servers - 1,
                                                                                                               player[
-                                                                                                                  'pos_x'],
+                                                                                                                  POS_X],
                                                                                                               player[
-                                                                                                                  'pos_y'],
+                                                                                                                  POS_Y],
                                                                                                               frontiers[
                                                                                                                   -1]))
-                except IndexError:
-                    change_player_server(server_list, player)
-            for i in range(len(frontiers) - 1):
-                if frontiers[i] <= player['pos_x'] < frontiers[i + 1]:
-                    try:
-                        server_list[i + 1]['bloom_filter'].add(player['id'])
-                        player['server'] = i + 1
+                else:
+                    raise IndexError
+            except IndexError:
+                change_player_server(server_list, player)
+
+        for i in range(len(frontiers) - 1):
+            if frontiers[i] <= player[POS_X] < frontiers[i + 1]:
+                try:
+                    if len(server_list[i + 1][HASH_SET]) < server_capacity:
+                        server_list[i + 1][HASH_SET].add(player[ID])
+                        player[SERVER] = i + 1
                         if verbose:
                             print("Player {} allocated in server {} - Coordinates({},{}) - Frontier: {} <= x < {}".format(
-                                player['id'], i + 1,
-                                player['pos_x'],
-                                player['pos_y'],
+                                player[ID], i + 1,
+                                player[POS_X],
+                                player[POS_Y],
                                 frontiers[i], frontiers[i + 1]))
-                    except IndexError:
-                        change_player_server(server_list, player)
+                    else:
+                        raise IndexError
+                except IndexError:
+                    change_player_server(server_list, player)
 
-    #TODO: implementar verbose e try/except, alem de alterar o plot para desenhar as fronteiras em ambos os eixos
-    elif sqrt(number_of_servers).is_integer() and number_of_servers >= 9:
-        frontiers = [[],[]]
-        is_grid = True
-        for i in range(sqrt(number_of_servers) - 1):
-            frontiers[0].append((i + 1) * (MAP_XMAX / sqrt(number_of_servers))) #Frontiers on the x-axis
-            frontiers[1].append((i + 1) * (MAP_YMAX / sqrt(number_of_servers))) #Frontiers on the y-axis
-        for player in player_list:
-            if player['pos_x'] < frontiers[0][0]:
-                if player['pos_y'] < frontier[1][0]:
-                    server_list[0]['bloom_filter'].add(player['id'])
-                    player['server'] = 0
-                if player['pos_y'] >= frontier[1][-1]:
-                    server_list[len(frontiers[0]) + 1]['bloom_filter'].add(player['id'])
-                    player['server'] = len(frontiers[0]) + 1
-            if player['pos_x'] >= frontiers[0][-1]:
-                if player['pos_y'] < frontiers[1][0]:
-                    server_list[len(frontiers[0]) + 2]['bloom_filter'].add(player['id'])
-                    player['server'] = len(frontiers[0]) + 2
-                if player['pos_y'] >= frontier[1][-1]:
-                    server_list[-1]['bloom_filter'].add(player['id'])
-                    player['server'] = len(server_list) - 1
-            for i in range(len(frontiers[0]) - 1):
-                if frontiers[0][i] <= player['pos_x'] < frontiers[0][i + 1]:
-                    for j in range(len(frontiers[1]) - 1):
-                        if frontiers[1][j] <= player['pos_y'] < frontiers[1][j + 1]:
-                            server[(i+1)*len(frontiers[0]) + (j+1)*len(frontiers[1])]['bloom_filter'].add(player['id'])
-                            player['server'] = (i+1)*len(frontiers[0]) + (j+1)*len(frontiers[1])
-
-    return frontiers, is_grid
+    return frontiers
             
         
 
 # Allocates players to a server based on the server location on the map (random spot and k nearest players are allocated to the server)
 def allocate_player_to_server_focus(player_list, server_list):
     for server in server_list:
-        server['pos_x'] = uniform(MAP_XMIN, MAP_XMAX)
-        server['pos_y'] = uniform(MAP_YMIN, MAP_YMAX)
+        server[POS_X] = uniform(0, MAP_XMAX)
+        server[POS_Y] = uniform(0, MAP_YMAX)
         if len(player_list) < floor(server_capacity * 0.8):
             k_nearest = len(player_list)
         else:
             k_nearest = floor(server_capacity * 0.8)
-        server['nearest_to_server'] = find_k_nearest(server['pos_x'], server['pos_y'], k_nearest)
+        server['nearest_to_server'] = find_k_nearest(server[POS_X], server[POS_Y], k_nearest)
     for player in player_list:
         player['nearest_server'] = 0
-        server_focus = np.array((server_list[0]['pos_x'], server_list[0]['pos_y']))
+        server_focus = np.array((server_list[0][POS_X], server_list[0][POS_Y]))
         shortest_distance = euclidean_distance(server_focus,
-                                               np.array((player['pos_x'], player['pos_y'])))
+                                               np.array((player[POS_X], player[POS_Y])))
         for node in server_list:
-            server_focus = np.array((node['pos_x'], node['pos_y']))
-            if player['id'] in node['nearest_to_server'] and 'server' not in player:
-                player_position = np.array((player['pos_x'], player['pos_y']))
+            server_focus = np.array((node[POS_X], node[POS_Y]))
+            if player[ID] in node['nearest_to_server'] and SERVER not in player:
+                player_position = np.array((player[POS_X], player[POS_Y]))
                 distance_to_focus = euclidean_distance(server_focus, player_position)
                 if distance_to_focus < shortest_distance:
                     shortest_distance = distance_to_focus
                     player['nearest_server'] = server_list.index(node)
     for player in player_list:
         try:
-            player['server'] = player['nearest_server']
-            server_list[player['server']]['bloom_filter'].add(player['id'])
+            player[SERVER] = player['nearest_server']
+            server_list[player[SERVER]][HASH_SET].add(player[ID])
             if verbose:
                 print(
                     "Player {} allocated in server {} - Server coordinates: ({},{}) - Player coordinates: ({},{})".format(
-                        player['id'], player['server'], server_list[player['server']]['pos_x'],
-                        server_list[player['server']]['pos_y'],
-                        player['pos_x'], player['pos_y']))
+                        player[ID], player[SERVER], server_list[player[SERVER]][POS_X],
+                        server_list[player[SERVER]][POS_Y],
+                        player[POS_X], player[POS_Y]))
         except IndexError:
-            del player['server']
+            del player[SERVER]
             change_player_server(server_list, player)
     return server_list
 
@@ -177,30 +170,30 @@ def allocate_player_to_server_focus(player_list, server_list):
 def change_player_server(server_list, player):
     emptiest_server = server_list[0]
     for server in server_list:
-        if server['bloom_filter'].count < emptiest_server['bloom_filter'].count:
+        if len(server[HASH_SET]) < len(emptiest_server[HASH_SET]):
             emptiest_server = server
-    emptiest_server['bloom_filter'].add(player['id'])
-    player['server'] = server_list.index(emptiest_server)
+    emptiest_server[HASH_SET].add(player[ID])
+    player[SERVER] = server_list.index(emptiest_server)
     if verbose:
-        print("Player {} reallocated to server {}".format(player['id'], player['server']))
+        print("Player {} reallocated to server {}".format(player[ID], player[SERVER]))
 
 
 # Calculates the list of viewable players by a single player
 def calculate_viewable_players(player_list, k):
     for player in player_list:
-        player['neighbors'] = find_k_nearest(player['pos_x'], player['pos_y'], k)
+        player[NEIGHBORS] = find_k_nearest(player[POS_X], player[POS_Y], k)
         if verbose:
-            print("{} nearest neighbors from player {}: {}".format(k, player['id'], player['neighbors']))
+            print("{} nearest neighbors from player {}: {}".format(k, player[ID], player[NEIGHBORS]))
 
 
 def publish_interest_groups(player_list, server_list):
     interest_groups = [BloomFilter(server_capacity, error_rate=0.1) for server in server_list]
     for player in player_list:
-        for neighbor_id in player['neighbors']:
-            if neighbor_id not in server_list[player['server']]['bloom_filter'] and neighbor_id not in interest_groups[player['server']]:
+        for neighbor_id in player[NEIGHBORS]:
+            if neighbor_id not in server_list[player[SERVER]][BLOOM_FILTER]:
                 if verbose:
-                    print("Player {} added to interest group of server {}".format(neighbor_id, player['server']))
-                interest_groups[player['server']].add(neighbor_id)
+                    print("Player {} added to interest group of server {}".format(neighbor_id, player[SERVER]))
+                interest_groups[player[SERVER]].add(neighbor_id)
     return interest_groups
 
 
@@ -214,13 +207,13 @@ def calculate_number_of_forwards_per_server(player_list, server_list):
     for i in range(number_of_servers):
         if verbose:
             print("Server {}: {} forwards".format(i, str(number_of_forwards_by_server[i])))
-    if 'pos_x' not in server_list[0]:
+    if POS_X not in server_list[0]:
         print("Total forwards: {}".format(sum(number_of_forwards_by_server)))
 
     return sum(number_of_forwards_by_server), number_of_forwards_by_server
 
 
-# Auxiliary function to calculate the euclidian distance between two points
+# Auxiliary function to calculate the euclidean distance between two points
 def euclidean_distance(a, b):
     return np.linalg.norm(a - b)
 
@@ -236,8 +229,8 @@ def hashing_method(players, servers):
 
 def equal_partitions_method(players, servers):
     idx = index.Index()
-    partitions, is_grid = allocate_player_to_server_equal_partitions(players, servers)
-    plot(players, "Método das partições", len(servers), partition=True, frontiers=partitions, grid=is_grid)
+    partitions= allocate_player_to_server_equal_partitions(players, servers)
+    plot(players, "Método das partições", len(servers), partition=True, frontiers=partitions)
     calculate_viewable_players(players, viewable_players)
     print("Método das partições: ")
     calculate_number_of_forwards_per_server(players, servers)
@@ -266,27 +259,21 @@ def server_focus_method(players, servers):
     print("Least number of forwards: {}".format(least_forwards))
 
 
-def plot(player_list, method_name, n_servers, partition=False, focus=False, grid=False, frontiers=[], servers=[]):
+def plot(player_list, method_name, n_servers, partition=False, focus=False, frontiers=[], servers=[]):
     cmap = plt.cm.get_cmap("hsv", n_servers+1)
     for player in player_list:
-        plt.scatter(player['pos_x'], player['pos_y'], c=cmap(player['server']))
+        plt.scatter(player[POS_X], player[POS_Y], c=cmap(player[SERVER]))
     plt.axis([0, MAP_XMAX, 0, MAP_YMAX])
     # for i, player in enumerate(player_list):
-    #     plt.annotate(xy=(player['pos_x'], player['pos_y']), s="Player " + str(i))
+    #     plt.annotate(xy=(player[POS_X], player[POS_Y]), s="Player " + str(i))
     if partition:
-        if grid:
-            for i, frontier in enumerate(frontiers):
-                plt.axvline(x=frontier, c=cmap(i+1))
-        else:
-            for i, frontier in enumerate(frontiers[0]):
-                plt.axvline(x=frontier, c=cmap(i+1))
-            for j, frontier in enumerate(frontiers[1]):
-                plt.axhline(y=frontier, c=cmap(len(frontiers[0])+(j+1)))
+        for i, frontier in enumerate(frontiers):
+            plt.axvline(x=frontier, c=cmap(i+1))
     if focus:
         for i, server in enumerate(servers):
-            plt.scatter(server['pos_x'], server['pos_y'], c=cmap(i), marker="s", s=100)
+            plt.scatter(server[POS_X], server[POS_Y], c=cmap(i), marker="s", s=100)
         # for i, server in enumerate(servers):
-        #     plt.annotate(xy=(server['pos_x'], server['pos_y']), s="Server " + str(i))
+        #     plt.annotate(xy=(server[POS_X], server[POS_Y]), s="Server " + str(i))
     plt.title(method_name)
     plt.grid(True)
     plt.show()
@@ -337,17 +324,17 @@ while True:
         verbose = bool(verbose)
         break
 list_of_players = generate_players(n_players)
-list_of_servers = create_servers(server_capacity, n_servers)
+list_of_servers = create_servers(n_servers)
 hashing = {}
 partitions = {}
 focus = {}
-hashing['players'] = deepcopy(list_of_players)
-hashing['servers'] = deepcopy(list_of_servers)
-partitions['players'] = deepcopy(list_of_players)
-partitions['servers'] = deepcopy(list_of_servers)
-focus['players'] = deepcopy(list_of_players)
-focus['servers'] = deepcopy(list_of_servers)
-hashing_method(hashing['players'], hashing['servers'])
-equal_partitions_method(partitions['players'], partitions['servers'])
-server_focus_method(focus['players'], focus['servers'])
+hashing[PLAYERS] = deepcopy(list_of_players)
+hashing[SERVERS] = deepcopy(list_of_servers)
+partitions[PLAYERS] = deepcopy(list_of_players)
+partitions[SERVERS] = deepcopy(list_of_servers)
+focus[PLAYERS] = deepcopy(list_of_players)
+focus[SERVERS] = deepcopy(list_of_servers)
+hashing_method(hashing[PLAYERS], hashing[SERVERS])
+equal_partitions_method(partitions[PLAYERS], partitions[SERVERS])
+server_focus_method(focus[PLAYERS], focus[SERVERS])
 
